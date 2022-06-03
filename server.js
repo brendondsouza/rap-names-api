65139270424f600bb6983fbc10b5fb8de2f11f89
Brendon DSouza <brendonmufc@gmail.com>
2022-06-02 20:59:51 -0400

    working rapper api

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+  '21 savage': {'age': 29,'birthName': 'Shéyaa Bin Abraham-Joseph','birthLocation': 'London, England'},
+  'chance the rapper': {'age': 29,'birthName': 'Chancelor Bennett','birthLocation': 'Chicago, Illinois'},
+  'unknown': {'age': 0,'birthName': 'Unknown', 'birthLocation': 'Unknown'}
+}
+
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/index.html')
+}) 
+
+app.get('/api/:name', (req,res)=> {
+  const rapperName = req.params.name.toLowerCase()
+  if(rappers[rapperName]){
+    res.json(rappers[rapperName])
+  } else{
+    res.json(rappers['unknown'])
+  }
+})
+
+app.listen(PORT, _ => {
+  console.log(`The server is running on port ${PORT}. You better go catch it!`)
+})
